162621a097465106e492f5e95100af9e91780036
Liubov <liubovusikova1@gmail.com>
2023-09-15 18:47:18 +0300

    Redux for filter items

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./slices/filterSlice";
+import filterReducer from "./slices/filterSlice";
 
 export const store = configureStore({
-	reducer: { counter: counterReducer },
+	reducer: { filterReducer },
 });
